f8884f5b00d33a6e08d8fab3e46b24288d3b5cab
rique <riquemello89@gmail.com>
2026-02-23 15:54:16 -0300

    fix: Criação da entidade NinjaModel, para nosso moldar nosso bd

--- src/main/java/CadastroDeNinjas01/User/Model/NinjaModel.java
+++ src/main/java/CadastroDeNinjas01/User/Model/NinjaModel.java
@@ -1,4 +1,23 @@
 package CadastroDeNinjas01.User.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_cadastro")
 public class NinjaModel {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column (name = "name")
+    private String name;
+    @Column (name = "email", unique = true)
+    private String email;
+    @Column (name = "idade")
+    private int idade;
 }
