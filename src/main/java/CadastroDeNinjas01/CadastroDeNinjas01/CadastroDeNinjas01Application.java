7afab8b439710cc22f4757de22b8418f206146a3
rique <riquemello89@gmail.com>
2026-02-23 14:08:01 -0300

    first commit

---
+++ src/main/java/CadastroDeNinjas01/CadastroDeNinjas01/CadastroDeNinjas01Application.java
@@ -0,0 +1,13 @@
+package CadastroDeNinjas01.CadastroDeNinjas01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjas01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjas01Application.class, args);
+	}
+
+}
---
+++ src/test/java/CadastroDeNinjas01/CadastroDeNinjas01/CadastroDeNinjas01ApplicationTests.java
@@ -0,0 +1,13 @@
+package CadastroDeNinjas01.CadastroDeNinjas01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjas01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
