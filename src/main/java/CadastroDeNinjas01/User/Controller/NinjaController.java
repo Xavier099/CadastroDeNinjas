a3fe585551692cb041fbee7d11f9d0cd9bbce158
rique <riquemello89@gmail.com>
2026-02-25 20:39:50 -0300

    feat(Controller): Cria endPoint para listar os usuarios

--- src/main/java/CadastroDeNinjas01/User/Controller/NinjaController.java
+++ src/main/java/CadastroDeNinjas01/User/Controller/NinjaController.java
@@ -1,15 +1,30 @@
 package CadastroDeNinjas01.User.Controller;
 
+import CadastroDeNinjas01.User.DTO.NinjaDTO;
+import CadastroDeNinjas01.User.Service.NInjaService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class NinjaController {
 
+    private NInjaService ninjaService;
+
+    public NinjaController(NInjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
     @GetMapping("/bem-vindo")
     public String bemVindo(){
         return "Ola seja bem vindo ao minha primeira rota";
+    }
+
+    @GetMapping("/list")
+    public List<NinjaDTO> list(){
+        return ninjaService.list();
     }
 }
