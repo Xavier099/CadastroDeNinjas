622a5f9de695128e5cb3114bffb4a1bb2172f16a
rique <riquemello89@gmail.com>
2026-02-25 20:39:11 -0300

    feat(Service): Cria logica para listar usuarios cadastrados

---
+++ src/main/java/CadastroDeNinjas01/User/Service/NInjaService.java
@@ -0,0 +1,29 @@
+package CadastroDeNinjas01.User.Service;
+
+import CadastroDeNinjas01.User.DTO.NinjaDTO;
+import CadastroDeNinjas01.User.DTO.NinjaMapper;
+import CadastroDeNinjas01.User.Model.NinjaModel;
+import CadastroDeNinjas01.User.Respository.NinjaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class NInjaService {
+    private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
+
+    public NInjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
+        this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
+    }
+
+    //list ninjas
+    public List<NinjaDTO> list(){
+        return ninjaRepository.findAll()
+                .stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
+    }
+}
